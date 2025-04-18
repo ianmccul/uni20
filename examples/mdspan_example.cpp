4a4e4fa14ff4570c890310e1d11d52fb7ae69f10
Ian McCulloch <ian@qusim.net>
2025-04-18 16:01:03 +0800

    Fix the mdspan namespace (stdex), and add stc/common/mdspan.cpp to get a consistent mdspan environment

--- examples/mdspan_example.cpp
+++ examples/mdspan_example.cpp
@@ -1,33 +1,36 @@
+
+#include "common/mdspan.hpp"
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <experimental/mdspan>
 #include <vector>
 
-namespace stdex = std::experimental;
+int main()
+{
+  // Create a vector to hold 3x3 matrix data.
+  std::vector<int> data(9);
+  for (int i = 0; i < 9; ++i)
+  {
+    data[i] = i; // Fill with values 0, 1, 2, ..., 8
+  }
 
-int main() {
-    // Create a vector to hold 3x3 matrix data.
-    std::vector<int> data(9);
-    for (int i = 0; i < 9; ++i) {
-        data[i] = i;  // Fill with values 0, 1, 2, ..., 8
+  // Create a 3x3 mdspan that wraps the vector.
+  // Here we use a dynamic extents type with fixed dimensions.
+  stdex::mdspan<int, stdex::extents<size_t, 3, 3>> matrix(data.data());
+
+  // Print the matrix using fmt.
+  fmt::print("Matrix (3x3):\n");
+  for (size_t i = 0; i < matrix.extents().extent(0); ++i)
+  {
+    for (size_t j = 0; j < matrix.extents().extent(1); ++j)
+    {
+      fmt::print("{:3d} ", matrix[i, j]);
     }
+    fmt::print("\n");
+  }
 
-    // Create a 3x3 mdspan that wraps the vector.
-    // Here we use a dynamic extents type with fixed dimensions.
-    stdex::mdspan<int, stdex::extents<size_t, 3, 3>> matrix(data.data());
+  // Also demonstrate fmt::format
+  auto formatted_str = fmt::format("The first element is: {}\n", matrix[0, 0]);
+  fmt::print("{}", formatted_str);
 
-    // Print the matrix using fmt.
-    fmt::print("Matrix (3x3):\n");
-    for (size_t i = 0; i < matrix.extents().extent(0); ++i) {
-        for (size_t j = 0; j < matrix.extents().extent(1); ++j) {
-            fmt::print("{:3d} ", matrix[i, j]);
-        }
-        fmt::print("\n");
-    }
-
-    // Also demonstrate fmt::format
-    auto formatted_str = fmt::format("The first element is: {}\n", matrix[0, 0]);
-    fmt::print("{}", formatted_str);
-
-    return 0;
+  return 0;
 }
---
+++ src/common/mdspan.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// use reference implementation
+#ifndef MDSPAN_IMPL_STANDARD_NAMESPACE
+#error "cmake configuration error: mdspan namespace is not defined"
+#endif
+
+#include <mdspan/mdspan.hpp>
