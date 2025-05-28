584c56c536fbc51d569864d08ad1352274b4392a
Ian McCulloch <ian@qusim.net>
2025-05-29 01:43:51 +0800

    Change implementation of async compound operators to use a += b, rather than a = a + b

--- src/async/async_ops.hpp
+++ src/async/async_ops.hpp
@@ -62,6 +62,7 @@ ValueAwaiter<std::remove_cvref_t<T>> read(T&& x)
   return ValueAwaiter<std::remove_cvref_t<T>>{std::forward<T>(x)};
 }
 
+// true if any of the arguments is Async<T> fomr some T
 template <typename... Args>
 constexpr bool contains_an_async = (false || ... || is_async<std::remove_cvref_t<Args>>::value);
 
@@ -87,20 +88,19 @@ template <typename T, typename U, typename Op>
 using async_binary_result_t =
     decltype(std::declval<Op>()(std::declval<async_value_t<T>>(), std::declval<async_value_t<U>>()));
 
-template <typename T, typename U, typename Op> struct is_async_binary_applicable : std::false_type
-{};
+/// \brief Concept to determine if Op(T,U) is a candidate for forwarding for Async
+template <typename T, typename U, typename Op>
+concept async_binary_applicable = (is_async_v<T> || is_async_v<U>) &&
+                                  requires(Op op, async_value_t<T> t, async_value_t<U> u) {
+                                    {
+                                      op(t, u)
+                                    };
+                                  };
 
 template <typename T, typename U, typename Op>
-  requires contains_an_async<T, U> && requires(Op op, async_value_t<T> t, async_value_t<U> u) {
-                                        {
-                                          op(t, u)
-                                        };
-                                      }
-struct is_async_binary_applicable<T, U, Op> : std::true_type
-{};
-
-template <typename T, typename U, typename Op>
-constexpr bool is_async_binary_applicable_v = is_async_binary_applicable<T, U, Op>::value;
+concept is_async_compound_applicable = is_async_v<T> && requires(async_value_t<T>& lhs, async_value_t<U>&& rhs, Op op) {
+                                                          op(lhs, std::forward<async_value_t<U>>(rhs));
+                                                        };
 
 // Example using move semantics
 // template <typename A, typename B, typename R, typename Op> void async_binary_op(A&& a, B&& b, Async<R>& out, Op op)
@@ -125,7 +125,6 @@ constexpr bool is_async_binary_applicable_v = is_async_binary_applicable<T, U, O
 /// \tparam R Result value type
 /// \tparam Op Callable with signature R(op(A, B))
 /// \ingroup async_api
-
 template <typename A, typename B, typename R, typename Op> void async_binary_op(A&& a, B&& b, Async<R>& out, Op op)
 {
   auto a_buf = read(std::forward<A>(a));
@@ -143,6 +142,31 @@ template <typename A, typename B, typename R, typename Op> void async_binary_op(
     co_await out_ = std::move(tmp); // Suspend *after* releasing readers
     co_return;
   }(std::move(a_buf), std::move(b_buf), std::move(out_buf), std::move(op)));
+}
+
+/// \brief Launch a coroutine applying an in-place operation op(lhs, rhs) on an Async<T>,
+///        where lhs is written, and rhs is read.
+///
+/// Schedules a coroutine that obtains the value in lhs as a reference, rhs as a const-reference,
+/// and executes `op(lhs, rhs)`. The coroutine suspends once to await both operands and
+/// then performs the in-place operation on lhs.
+///
+/// \tparam U Right-hand operand type (scalar or Async<U>)
+/// \tparam T Value type of the Async<T> being modified
+/// \tparam Op Callable with signature void(T&, U const&)
+/// \param rhs Right-hand operand (either U or Async<U>)
+/// \param lhs Left-hand operand of the form Async<T>& — this is the value being modified
+/// \param op  In-place operation to apply (e.g., a lambda using operator+=)
+/// \ingroup async_api
+template <typename U, typename T, typename Op> void async_compound_op(U&& rhs, Async<T>& lhs, Op op)
+{
+  schedule([](auto rhs_, WriteBuffer<T> out_, Op op_) -> AsyncTask {
+    auto tmp = co_await all(rhs_, out_);
+    auto& rhs_val = std::get<0>(tmp);
+    auto& lhs_ref = std::get<1>(tmp);
+    op_(lhs_ref, rhs_val); // in-place mutation
+    co_return;
+  }(read(std::forward<U>(rhs)), lhs.write(), std::move(op)));
 }
 
 // This version is correct, but does not use all() awaiter
@@ -186,7 +210,7 @@ void async_assign(U&& rhs, Async<T>& lhs)
 // +, -, *, /
 #define UNI20_DEFINE_BINARY_OP(OPNAME, OP)                                                                             \
   template <typename T, typename U>                                                                                    \
-    requires is_async_binary_applicable_v<T, U, OP> && contains_an_async<T, U>                                         \
+    requires async_binary_applicable<T, U, OP>                                                                         \
   auto operator OPNAME(T&& a, U&& b)                                                                                   \
   {                                                                                                                    \
     using R = async_binary_result_t<T, U, OP>;                                                                         \
@@ -202,22 +226,38 @@ UNI20_DEFINE_BINARY_OP(/, std::divides<>)
 
 #undef UNI20_DEFINE_BINARY_OP
 
-// +=, -=, *=, /=
-#define UNI20_DEFINE_COMPOUND_OP(OPNAME, OP)                                                                           \
-  template <typename T, typename U>                                                                                    \
-    requires is_async_binary_applicable_v<T, U, OP> && contains_an_async<T, U>                                         \
-  Async<T>& operator OPNAME##=(Async<T>& lhs, U&& rhs)                                                                 \
+#define UNI20_DEFINE_ASSIGN_OP(NAME, OP)                                                                               \
+  struct NAME                                                                                                          \
   {                                                                                                                    \
-    async_binary_op(lhs, std::forward<U>(rhs), lhs, OP{});                                                             \
+      template <typename LHS, typename RHS> void operator()(LHS& lhs, RHS&& rhs) const                                 \
+      {                                                                                                                \
+        lhs OP std::forward<RHS>(rhs);                                                                                 \
+      }                                                                                                                \
+  };
+
+UNI20_DEFINE_ASSIGN_OP(plus_assign, +=)
+UNI20_DEFINE_ASSIGN_OP(minus_assign, -=)
+UNI20_DEFINE_ASSIGN_OP(multiplies_assign, *=)
+UNI20_DEFINE_ASSIGN_OP(divides_assign, /=)
+
+#undef UNI20_DEFINE_ASSIGN_OP
+
+#define UNI20_DEFINE_ASYNC_COMPOUND_OPERATOR(OPSYM, FUNCTOR)                                                           \
+  template <typename T, typename U>                                                                                    \
+    requires uni20::async::is_async_compound_applicable<U, T, uni20::async::FUNCTOR>                                   \
+  Async<T>& operator OPSYM(Async<T>& lhs, U&& rhs)                                                                     \
+  {                                                                                                                    \
+    uni20::async::async_compound_op(std::forward<U>(rhs), lhs, uni20::async::FUNCTOR{});                               \
     return lhs;                                                                                                        \
   }
 
-UNI20_DEFINE_COMPOUND_OP(+, std::plus<>)
-UNI20_DEFINE_COMPOUND_OP(-, std::minus<>)
-UNI20_DEFINE_COMPOUND_OP(*, std::multiplies<>)
-UNI20_DEFINE_COMPOUND_OP(/, std::divides<>)
+UNI20_DEFINE_ASYNC_COMPOUND_OPERATOR(+=, plus_assign)
+UNI20_DEFINE_ASYNC_COMPOUND_OPERATOR(-=, minus_assign)
+UNI20_DEFINE_ASYNC_COMPOUND_OPERATOR(*=, multiplies_assign)
+UNI20_DEFINE_ASYNC_COMPOUND_OPERATOR(/=, divides_assign)
 
-#undef UNI20_DEFINE_COMPOUND_OP
+#undef UNI20_DEFINE_ASYNC_COMPOUND_OPERATOR
+
 /// @}
 
 } // namespace uni20::async
