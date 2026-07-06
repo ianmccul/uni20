57953152ac065a0cc882f38cb242f0af7a2abf7a
Ian McCulloch <ian@qusim.net>
2026-07-06 13:50:37 +0800

    Fix mdspan target propagation

--- src/uni20/common/mdspan.hpp
+++ src/uni20/common/mdspan.hpp
@@ -6,6 +6,8 @@
  * \brief Wrapper that exposes the mdspan reference implementation and a formatter bridge.
  */
 
+#include <uni20/config.hpp>
+
 // use reference implementation
 #ifndef MDSPAN_IMPL_STANDARD_NAMESPACE
 #error "cmake configuration error: mdspan namespace is not defined"
